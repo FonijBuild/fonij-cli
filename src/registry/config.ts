342520cfa824a6ddd949f636c30fc713db2a2405
Fonij2002 <fonij2002@gmail.com>
2026-09-15 22:22:44 +0330

    Remove registry

--- src/registry/config.ts
+++
@@ -1,6 +0,0 @@
-export const REGISTRY_URL =
-  "https://raw.githubusercontent.com/FonijBuild/fonij-registry/main/starters.json";
-
-export const REGISTRY_CACHE_KEY = "fonij-registry";
-
-export const CACHE_DURATION = 1000 * 60 * 60 * 24;
