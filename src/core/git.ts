896837814d5683b5bb8cbab8bd2e27d91a43ea9e
Fonij2002 <fonij2002@gmail.com>
2026-09-15 22:23:03 +0330

    Remove core

--- src/core/git.ts
+++
@@ -1,9 +0,0 @@
-import { execa } from "execa";
-
-export async function cloneRepository(url: string, destination: string) {
-  await execa("git", ["clone", url, destination]);
-}
-
-export async function initGit() {
-  await execa("git", ["init"]);
-}
